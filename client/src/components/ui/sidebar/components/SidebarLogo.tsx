c0e5fdb32546fcf25ca858cca480863de1ab3b82
atullya <atulmzn1@gmail.com>
2026-01-06 17:00:32 +0545

    fix logo

--- client/src/components/ui/sidebar/components/SidebarLogo.tsx
+++ client/src/components/ui/sidebar/components/SidebarLogo.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-
+import logo1 from "../../../../assets/logo1.svg";
+import logo2 from "../../../../assets/logo2.svg";
 interface SidebarLogoProps {
   className?: string;
 }
@@ -8,19 +9,11 @@ export const SidebarLogo: React.FC<SidebarLogoProps> = ({ className = "" }) => {
   return (
     <div className={`w-[27px] h-[24px] relative flex ${className}`}>
       <div className="absolute top-0 left-0 w-[12.1659px] h-[16.3524px]">
-        <img
-          src="/src/assets/logo1.svg"
-          alt="logo-left"
-          className="w-full h-full"
-        />
+        <img src={logo1} alt="logo-left" className="w-full h-full" />
       </div>
 
       <div className="absolute top-[0.02px] left-[10.67px] w-[16.3316px] h-[24.0334px]">
-        <img
-          src="/src/assets/logo2.svg"
-          alt="logo-right"
-          className="w-full h-full"
-        />
+        <img src={logo2} alt="logo-right" className="w-full h-full" />
       </div>
     </div>
   );
